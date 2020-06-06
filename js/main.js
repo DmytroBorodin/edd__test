eb5a63be2c716e26288517f1b6575a5ea4817663
Dmytro Borodin <48290702+DmytroBorodin@users.noreply.github.com>
2020-06-06 15:05:54 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,88 @@
+'use strict'
+
+let mainInput = document.querySelector('#main__input');
+let mainInputMin = document.querySelector('#main__input-min');
+let titleBlock = document.querySelector('.title__block');
+let imgBlock = document.querySelector('.img__block');
+let wrap = document.querySelector('.wrap');
+let mainContent = document.querySelector('.main__content');
+let countriesBlock = document.querySelector('.countries__block');
+let tipsBlock = document.querySelector('.tips__block');
+let tipsBlockMini = document.querySelector('#tips__block__500');
+let navBar = document.querySelector('.navbar');
+let img = document.getElementById('img');
+let title = document.querySelector('.page__title');
+let subTitle = document.querySelector('.sub__title');
+console.log(mainInput.value);
+
+mainInput.addEventListener('focus', () => {
+	countriesBlock.classList.add('invisible');
+})
+mainInput.addEventListener('blur', () => {
+	countriesBlock.classList.remove('invisible');
+	tipsBlock.classList.add('invisible');
+})
+
+mainInputMin.addEventListener('focusin', () => {
+  window.scroll({
+    top: 100,
+    left: 100,
+    behavior: 'smooth'
+  });
+	title.classList.add('invisible');
+	img.classList.add('img__pos');
+	subTitle.classList.add('invisible');
+	mainContent.classList.add('section__mt');
+});
+
+mainInputMin.addEventListener('focusout', () => {
+	title.classList.remove('invisible');
+  img.classList.remove('img__pos');
+  subTitle.classList.remove('invisible');
+	mainContent.classList.remove('section__mt');
+	tipsBlockMini.classList.add('invisible');
+})
+
+function filterFunction() {
+	let filter = mainInput.value.toUpperCase();
+	let p = tipsBlock.getElementsByTagName("p");
+	tipsBlock.classList.remove('invisible');
+    for (let i = 0; i < p.length; i++) {
+    	let txtValue = p[i].innerHTML;
+    	if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      		p[i].classList.remove('invisible');
+  		}else {
+      		p[i].classList.add('invisible');
+    	}
+  	}
+  	let pArray = [...p];
+  	pArray.forEach((item) => {
+  		item.addEventListener('mouseover', () => {
+  			mainInput.value = item.innerHTML;
+  		})
+  	})
+}
+
+
+function filterFunctionMini() {
+	let filter = mainInputMin.value.toUpperCase();
+	let p = tipsBlockMini.getElementsByTagName("p");
+	tipsBlockMini.classList.remove('invisible');
+    for (let i = 0; i < p.length; i++) {
+    	let txtValue = p[i].innerHTML;
+    	if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      		p[i].classList.remove('invisible');
+  		}else {
+      		p[i].classList.add('invisible');
+    	}
+  	}
+  	let pArray = [...p];
+  	console.log(pArray);
+  	pArray.forEach((item) => {
+  		item.addEventListener('mouseover', () => {
+  			mainInputMin.value = item.innerHTML;
+  		})
+  	})
+}
+
+
